c483dd0d5d258e47b0cf06242c9d2169aea892e9
pranjalsh22 <physics0403@gmail.com>
2025-03-01 18:00:30 +0530

    Update timeline.py

--- timeline.py
+++ timeline.py
@@ -7,11 +7,12 @@ import os
 load_dotenv()
 
 # Database connection details
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DB_NAME=timeline
+DB_USER=postgres
+DB_PASSWORD=pranjalpalak
+DB_HOST=db.xlabuzvmqnuqtrkfyxif.supabase.co
+DB_PORT=5432
+PASSCODE=pranjalpalak
 
 # Connect to PostgreSQL database
 def get_connection():

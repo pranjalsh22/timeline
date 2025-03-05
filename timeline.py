f274f0471ba25da24365efaa9bdd0b9910b618fc
pranjalsh22 <physics0403@gmail.com>
2025-03-05 12:43:33 +0530

    Update timeline.py

--- timeline.py
+++ timeline.py
@@ -75,12 +75,20 @@ def fetch_entries():
 def display_timeline():
     entries = fetch_entries()
     for entry in entries:
-        st.write(f"**{entry[3]}** ({entry[2]})")
-        st.write(f"**Scientist:** {entry[1]}")
-        st.write(f"**Description:** {entry[4]}")
-        st.write(f"**Links:** {entry[5]}")
-        st.write(f"**Tags:** {entry[6]}")
-        st.write("---")
+        st.write({entry[0]})
+        st.write({entry[1]})
+        st.write({entry[2]})
+        st.write({entry[3]})
+        st.write({entry[4]})
+        st.write({entry[5]})
+        st.write({entry[6]})
+        
+        #st.write(f"**{entry[3]}** ({entry[2]})")
+        #st.write(f"**Scientist:** {entry[1]}")
+        #st.write(f"**Description:** {entry[4]}")
+        #st.write(f"**Links:** {entry[5]}")
+        #st.write(f"**Tags:** {entry[6]}")
+        #st.write("---")
 
 # Main app function
 def main():
